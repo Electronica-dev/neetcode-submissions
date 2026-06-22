ab4547aa374c2513f593f79ce22bf708595fc400
Samartha <45600001+Electronica-dev@users.noreply.github.com>
2026-06-22 17:39:22 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for word in strs:
+            key = [0] * 26
+            for letter in word:
+                key[ord(letter) - 97] += 1
+            res[tuple(key)].append(word)
+        return list(res.values())
+            
